b9f7e03a7c396e6513da0c877229d024e063243f
Tomas Kalibera <kalibera@cs.purdue.edu>
2012-09-12 13:20:27 +0200

    subtraction (still scalars only)

---
+++ src/r/nodes/Sub.java
@@ -0,0 +1,14 @@
+package r.nodes;
+
+@PrettyName("-")
+@Precedence(Operation.SUB_PRECEDENCE)
+public class Sub extends BinaryOperation {
+    public Sub(ASTNode l, ASTNode r) {
+        super(l, r);
+    }
+
+    @Override
+    public void accept(Visitor v) {
+        v.visit(this);
+    }
+}
