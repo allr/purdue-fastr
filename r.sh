af74bc4202061768153ce19791b466e0e813828a
Tomas Kalibera <kalibera@cs.purdue.edu>
2012-09-26 18:02:26 +0200

    added classpath for truffle/graal compilers

--- r.sh
+++ r.sh
@@ -8,5 +8,23 @@ export JAVA_HOME=/opt/jdk7
 # -------------------------------
 
 export PATH=$JAVA_HOME/bin:$PATH
-java -cp $FH/antlr-runtime-3.4.jar:$FH/fastr/bin:$FH/graalvm-truffle/graal/com.oracle.truffle/bin r.Console $*
+
+function bpath() {
+
+  for P in $* ; do
+    echo -n ":$FH/graalvm-truffle/graal/"$P"/bin"
+  done
+
+}
+
+export PATH=$JAVA_HOME/bin:$PATH
+
+CP=`bpath \
+	com.oracle.truffle com.oracle.truffle.compiler com.oracle.graal.api.code com.oracle.graal.api.meta com.oracle.graal.compiler com.oracle.graal.java \
+	com.oracle.graal.hotspot com.oracle.graal.api com.oracle.max.asm com.oracle.max.cri com.oracle.graal.nodes com.oracle.graal.debug \
+`
+
+java -cp $FH/antlr-runtime-3.4.jar:$FH/fastr/bin:$CP r.Console $*
+
+
 
--- rg.sh
+++ rg.sh
@@ -8,4 +8,20 @@ export JAVA_HOME=/home/tomas/hg/graalvm-truffle/jdk1.7.0_06/product
 # -------
 
 export PATH=$JAVA_HOME/bin:$PATH
-java -cp $FH/antlr-runtime-3.4.jar:$FH/fastr/bin:$FH/graalvm-truffle/graal/com.oracle.truffle/bin -graal r.Console $*
+
+function bpath() {
+
+  for P in $* ; do
+    echo -n ":$FH/graalvm-truffle/graal/"$P"/bin"
+  done
+
+}
+
+export PATH=$JAVA_HOME/bin:$PATH
+
+CP=`bpath \
+	com.oracle.truffle com.oracle.truffle.compiler com.oracle.graal.api.code com.oracle.graal.api.meta com.oracle.graal.compiler com.oracle.graal.java \
+	com.oracle.graal.hotspot com.oracle.graal.api com.oracle.max.asm com.oracle.max.cri com.oracle.graal.nodes com.oracle.graal.debug \
+`
+
+java -cp $FH/antlr-runtime-3.4.jar:$FH/fastr/bin:$CP -graal r.Console $*
