b74165557b19e1b6ee9ae81640891a6ad37bea0a
Tomas Kalibera <kalibera@cs.purdue.edu>
2012-10-04 17:20:36 +0200

    running fannkuch from mx script

--- mx/commands.py
+++ mx/commands.py
@@ -1,5 +1,5 @@
 
-import os, sys;
+import os, sys, shutil;
 from os.path import exists, join;
 import mx;
 
@@ -12,7 +12,9 @@ def mx_init():
   commands = {
       'frtest': [frtest, ''],
       'r': [rconsoleServer, ''],
-      'rg': [rconsoleGraal, '']
+      'rg': [rconsoleGraal, ''],
+      'rfannkuch': [rfannkuchServer, '[size]'],
+      'rgfannkuch': [rfannkuchGraal, '[size]']
   }
   mx.commands.update(commands);
 
@@ -28,7 +30,8 @@ def mx_init():
 
   global gmod;
   gmod = mod;
-  
+
+# ------------------  
 
 def frtest(args):
   """Test FastR MX target"""
@@ -41,14 +44,42 @@ def frtest(args):
   
 def rconsoleServer(args):
   """Run R Console with the HotSpot server VM"""  
-  rconsole([], 'server')
+  rconsole([], 'server', [])
   
 def rconsoleGraal(args):
   """Run R Console with the Graal VM"""  
-  rconsole(['-XX:-BootstrapGraal'], 'graal')
-  
-def rconsole(vmArgs, vm):
-  """Run R Console with the given VM"""
-  gmod.vm( vmArgs + ['-cp', mx.classpath("fastr") , 'r.Console' ], vm = vm ); 
+  rconsole(['-XX:-BootstrapGraal'], 'graal', [])
 
+def rfannkuchServer(args):
+  """Run Fannkuch with the HotSpot server VM"""  
+  rfannkuch(args, [], 'server')
+
+def rfannkuchGraal(args):
+  """Run Fannkuch with the Graal VM"""  
+  rfannkuch(args, ['-XX:-BootstrapGraal'], 'graal')
+
+# ------------------
+  
+def rconsole(vmArgs, vm, cArgs):
+  """Run R Console with the given VM"""
+  gmod.vm( vmArgs + ['-cp', mx.classpath("fastr") , 'r.Console' ] + cArgs, vm = vm ); 
+
+def rfannkuch(args, vmArgs, vm):
+  """Run Fannkuch benchmark using the given VM"""
+  if (len(args)==0):
+    size = '6L';
+  else:
+    size = args[0] + 'L';
+
+  source = join(os.getcwd(), "..", "fastr", "test", "r", "shootout", "fannkuch", "fannkuchredux.r"); 
+  tmp = ".tmp.fannkuch.torun.r";
+
+  shutil.copyfile(source, tmp);
+  with open(tmp, "a") as f:
+    f.write("fannkuch("+size+")\n");
+        
+  print("Problem size "+size);
+  print("Input file "+tmp);
+  
+  rconsole(vmArgs, vm, [tmp]);
   
