6d09f13dc3b0da68133081a283ed79bf8d25f348
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-01-16 16:41:52 +0100

    ascii version of mandelbrot as unit test

--- test/r/shootout/mandelbrot/TestMandelbrot.java
+++ test/r/shootout/mandelbrot/TestMandelbrot.java
@@ -9,6 +9,20 @@ import r.shootout.*;
 public class TestMandelbrot extends ShootoutTestBase {
     @Test
     public void testMandelbrot() {
-        assertShootout("mandelbrot", "mandelbrot", 100, null, null, "NULL"); // TODO: check the output !!!
+        assertShootout("mandelbrot", "mandelbrot", 100, null, null, "NULL"); // TODO: check the output (its a binary output from a child process)
+        assertShootout("mandelbrot", "mandelbrot-ascii", 10,
+                        "P4\n" +
+                        "10 10 \n" +
+                        "00 00 \n" +
+                        "01 00 \n" +
+                        "07 80 \n" +
+                        "07 c0 \n" +
+                        "3f c0 \n" +
+                        "ff 80 \n" +
+                        "3f c0 \n" +
+                        "07 c0 \n" +
+                        "07 80 \n" +
+                        "01 00 \n"
+        	, null, "NULL");
     }
 }
