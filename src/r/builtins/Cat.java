bda940f8d2406050ad287c3ee2d3f5100807f1ea
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-02-25 14:15:51 +0100

    cat optimization

--- src/r/builtins/Cat.java
+++ src/r/builtins/Cat.java
@@ -8,12 +8,15 @@ import com.oracle.truffle.api.nodes.*;
 import r.*;
 import r.builtins.BuiltIn.NamedArgsBuiltIn.*;
 import r.data.*;
+import r.data.internal.*;
 import r.errors.*;
 import r.nodes.*;
 import r.nodes.truffle.*;
 
 // FIXME: not quite the GNU-R output particularly for numbers; re-visit if some code could be removed once we have fully GNU-R string representation of numerics
 public class Cat {
+
+    private static final int DEFAULT_BUFFER_SIZE = 4096;
 
     private static final String[] paramNames = new String[]{"...", "sep"};
     private static final int ISEP = 1;
@@ -136,7 +139,7 @@ public class Cat {
     // speculates on that all arguments are strings and separator is an empty string
     // the empty separator is a usual thing in R programs
     // all args strings is inspired by fasta
-    public static void catStrings(PrintWriter out, RAny[] args, int sepArgPos, ASTNode ast) throws UnexpectedResultException {
+    public static void catStringsBuilder(PrintWriter out, RAny[] args, int sepArgPos, ASTNode ast) throws UnexpectedResultException {
         if (sepArgPos == -1) {
             throw new UnexpectedResultException(null);
         }
@@ -163,6 +166,77 @@ public class Cat {
         out.flush();
     }
 
+
+    private static char[] buffer = new char[DEFAULT_BUFFER_SIZE];
+
+    // a (slightly) faster version of catStringsBuilder
+    public static void catStrings(PrintWriter out, RAny[] args, int sepArgPos, ASTNode ast) throws UnexpectedResultException {
+        if (sepArgPos == -1) {
+            throw new UnexpectedResultException(null);
+        }
+        int argslen = args.length;
+        int bufPos = 0;
+        for (int j = 0; j < argslen; j++) {
+            RAny arg = args[j];
+            if (!(arg instanceof RString)) {
+                throw new UnexpectedResultException(null);
+            }
+            RString rs = (RString) arg;
+            int size = rs.size();
+            if (j != sepArgPos) {
+                for (int i = 0; i < size; i++) {
+                    String as = rs.getString(i);
+                    int asSize = as.length();
+                    if (bufPos + asSize > buffer.length) {
+                        int newSize = buffer.length * 2;
+                        while (bufPos + asSize > newSize) {
+                            newSize *= 2;
+                        }
+                        char[] oldBuffer = buffer;
+                        buffer = new char[newSize];
+                        System.arraycopy(oldBuffer, 0, buffer, 0, bufPos);
+                    }
+                    for (int k = 0; k < asSize; k++) {
+                        buffer[bufPos++] = as.charAt(k);
+                    }
+                }
+            } else {
+                if (size != 1 || rs.getString(0).length() > 0) {
+                    throw new UnexpectedResultException(null);
+                }
+            }
+        }
+        out.write(buffer, 0, bufPos);
+        out.flush();
+    }
+
+    // another (slightly) faster version of catStringsBuilder
+    // NOTE: we could optimize even more, e.g. specialize for a node with just one scalar string and then a constant (empty) separator and a constant newline
+    // argument; however, it is unlikely to help much, if printing a lot and doing it line-by-line, we cannot really help much
+    // an optimization to do a lazy flush could help more
+
+    public static void catScalarStringsNoCopy(PrintWriter out, RAny[] args, int sepArgPos, ASTNode ast) throws UnexpectedResultException {
+        if (sepArgPos == -1) {
+            throw new UnexpectedResultException(null);
+        }
+        int argslen = args.length;
+        for (int j = 0; j < argslen; j++) {
+            RAny arg = args[j];
+            if (!(arg instanceof ScalarStringImpl)) {
+                throw new UnexpectedResultException(null);
+            }
+        }
+        String sep = ((ScalarStringImpl) args[sepArgPos]).getString();
+        if (sep.length() != 0) {
+            throw new UnexpectedResultException(null);
+        }
+        for (int j = 0; j < argslen; j++) {
+            String str = ((ScalarStringImpl) args[j]).getString();
+            out.write(str);
+        }
+        out.flush();
+    }
+
     public static final CallFactory FACTORY = new CallFactory() {
         @Override
         public RNode create(ASTNode call, RSymbol[] names, RNode[] exprs) {
@@ -183,13 +257,15 @@ public class Cat {
 
             final int sepPosition = provided[ISEP] ? paramPositions[ISEP] : -1;
             final PrintWriter stdOut = new PrintWriter(System.out, true); // stdout buffering, important for fasta
+
             return new BuiltIn(call, names, exprs) {
 
                 @Override
                 public final RAny doBuiltIn(Frame frame, RAny[] params) {
                     // assume we are only printing strings and separator is an empty (single-element) string
                     try {
-                        catStrings(stdOut, params, sepPosition, ast);
+//                        catStrings(stdOut, params, sepPosition, ast);
+                        catScalarStringsNoCopy(stdOut, params, sepPosition, ast);
                     } catch (UnexpectedResultException e) {
                         RNode generic = new BuiltIn(ast, argNames, argExprs) {
                             @Override
