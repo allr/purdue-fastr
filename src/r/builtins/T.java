e3f3aef8e76a50a456d4ec2c02b6814ee0c10f19
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-04-19 16:23:26 +0200

    cache friendly square matrix transposition

--- src/r/builtins/T.java
+++ src/r/builtins/T.java
@@ -50,7 +50,7 @@ final class T extends CallFactory {
                         int n = dim[1];
                         int[] ndim = new int[]{n, m};
                         if (IN_PLACE && a.isTemporary() && m == n) { // note: a view is not temporary
-                            inPlaceRectangular(a, m);
+                            inPlaceSquare(a, m);
                             return a.setNames(null).setDimensions(ndim);
                         } else {
                             RArray res = Utils.createArray(a, size, null, null, a.attributesRef());
@@ -61,6 +61,10 @@ final class T extends CallFactory {
 //                                    res.set(i * n + j, a.getRef(j * m + i));
 //                                }
 //                            }
+                            if (n > 1000 && m == n) {  // FIXME: a tuning parameter
+                                square(a, n, res);
+                                return res.setDimensions(ndim);
+                            }
                             int j = 0;  // LICENSE: transcribed from GNU-R, which is licensed under GPL
                             int size1 = size - 1;
                             for (int i = 0; i < size ; i++) {
@@ -79,7 +83,7 @@ final class T extends CallFactory {
         };
     }
 
-    public static void inPlaceRectangular(RArray a, int m) {
+    public static void inPlaceSquare(RArray a, int m) {
         for (int i = 0; i < m - 1; i++) {
             for (int j = i + 1; j < m; j++) {
                 int first = i * m + j;
@@ -93,7 +97,7 @@ final class T extends CallFactory {
     }
 
     // note: not any faster than inPlaceRectangular
-    public static void inPlaceRectangular(DoubleImpl adbl, int m) {
+    public static void inPlaceSquare(DoubleImpl adbl, int m) {
         double[] a = adbl.getContent();
         for (int i = 0; i < m - 1; i++) {
             for (int j = i + 1; j < m; j++) {
@@ -103,6 +107,79 @@ final class T extends CallFactory {
                 double tmp = a[first];
                 a[first] = a[second];
                 a[second] = tmp;
+            }
+        }
+    }
+
+    public static void square(RArray a, int n, RArray res) {
+
+        final int delta = n > 1000 ? 40 : 0; // FIXME: the choice of 40 is probably quite fragile wrt to performance
+                                             // should better do some auto-tuning or a self-oblivious algorithm
+        final int size = n * n;
+        final int size1 = size - 1;
+        int i = 0;
+        int j = 0;
+
+        // will be transposing by blocks delta x delta
+        // reading a by rows, writing to a by columns, block-wise
+        // i is offset into res, j is offset into a
+        final int blocksPerSide = delta > 0 ? n / delta : 0;
+        final int epsilon = n - blocksPerSide * delta;
+
+        final int iadvance1 = n - delta;
+        final int jadvance1 = - ((delta * n) - 1);
+        final int iadvance2 = - (delta * (n - 1));
+        final int jadvance2 = delta * (n - 1);
+        final int iadvance3 = (delta - 1) * n + epsilon;
+        final int jadvance3 = -((n - epsilon) * n - delta);
+
+        for (int bcol = 0; bcol < blocksPerSide; bcol++) {
+            for (int brow = 0; brow < blocksPerSide; brow++) {
+                for (int icol = 0; icol < delta; icol++) {
+                    for (int irow = 0; irow < delta; irow++) {
+                        res.set(i, a.getRef(j));
+                        i ++;
+                        j += n;
+                    }
+                    i += iadvance1;
+                    j += jadvance1;
+                }
+                i += iadvance2;
+                j += jadvance2;
+            }
+            i += iadvance3;
+            j += jadvance3;
+        }
+
+
+        // transponse the remainder of the matrix
+
+        if (epsilon > 0) {
+            // transpose the lower remainder (epsilon * n) into the right remainder (n * epsilon)
+            i = (n - epsilon) * n;
+            j = n - epsilon;
+            for (; i < size; i++) {
+                res.set(i, a.getRef(j));
+                j += n;
+                if (j > size1) {
+                    j -= size1;
+                }
+            }
+
+            // transpose the upper-right remainder ( (n-epsilon) * epsilon) into the bottom-left remainder (epsilon * (n-epsilon))
+            i = n - epsilon;
+            j = (n - epsilon) * n;
+            final int iend = j;
+            final int rsize1 = n * epsilon - 1;
+            final int rowoffset = n - epsilon;
+            for (;i < iend;) {
+                res.set(i, a.getRef(j));
+                j += n;
+                i ++;
+                if (j > size1) {
+                    j -= rsize1;
+                    i += rowoffset;
+                }
             }
         }
     }
