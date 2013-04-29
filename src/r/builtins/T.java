1c6f75e3b84d0dce242c9f1efe364a00195b121a
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-04-29 18:47:07 +0200

    specialized square matrix transpose for int and double

--- src/r/builtins/T.java
+++ src/r/builtins/T.java
@@ -52,30 +52,38 @@ final class T extends CallFactory {
                         if (IN_PLACE && a.isTemporary() && m == n) { // note: a view is not temporary
                             inPlaceSquare(a, m);
                             return a.setNames(null).setDimensions(ndim);
-                        } else {
-                            RArray res = Utils.createArray(a, size, null, null, a.attributesRef());
+                        }
+                        boolean blocked = n > 1000 && m == n; // FIXME: a tuning parameter
 
-// naive version is slower than the R version below
-//                            for (int i = 0; i < m; i++) {
-//                                for (int j = 0; j < n; j++) {
-//                                    res.set(i * n + j, a.getRef(j * m + i));
-//                                }
-//                            }
-                            if (n > 1000 && m == n) {  // FIXME: a tuning parameter
-                                square(a, n, res);
+                        if (blocked) {
+                            if (a instanceof RDouble) {
+                                double[] content = new double[size];
+                                squareBlocked(((RDouble) a).getContent(), n, content);
+                                return RDouble.RDoubleFactory.getFor(content, null, null, a.attributesRef());
+                            } else if (a instanceof RInt) {
+                                int[] content = new int[size];
+                                squareBlocked(((RInt) a).getContent(), n, content);
+                                return RInt.RIntFactory.getFor(content, null, null, a.attributesRef());
+                            } else {
+                                RArray res = Utils.createArray(a, size, null, null, a.attributesRef());
+                                squareBlocked(a, n, res);
                                 return res.setDimensions(ndim);
                             }
-                            int j = 0;  // LICENSE: transcribed from GNU-R, which is licensed under GPL
-                            int size1 = size - 1;
-                            for (int i = 0; i < size ; i++) {
-                                res.set(i, a.getRef(j));
-                                j += m;
-                                if (j > size1) {
-                                    j -= size1;
-                                }
-                            }
-                            return res.setDimensions(ndim);
                         }
+
+                        RArray res = Utils.createArray(a, size, null, null, a.attributesRef());
+
+                            // TODO: re-check that type specialization won't help here
+                        int j = 0;  // LICENSE: transcribed from GNU-R, which is licensed under GPL
+                        int size1 = size - 1;
+                        for (int i = 0; i < size ; i++) {
+                            res.set(i, a.getRef(j));
+                            j += m;
+                            if (j > size1) {
+                                j -= size1;
+                            }
+                        }
+                        return res.setDimensions(ndim);
                     }
                 }
                 throw RError.getArgumentNotMatrix(ast);
@@ -97,6 +105,7 @@ final class T extends CallFactory {
     }
 
     // note: not any faster than inPlaceRectangular
+    // TODO: revisit - revalidate
     public static void inPlaceSquare(DoubleImpl adbl, int m) {
         double[] a = adbl.getContent();
         for (int i = 0; i < m - 1; i++) {
@@ -111,7 +120,7 @@ final class T extends CallFactory {
         }
     }
 
-    public static void square(RArray a, int n, RArray res) {
+    public static void squareBlocked(RArray a, int n, RArray res) {
 
         final int delta = n > 1000 ? 40 : 0; // FIXME: the choice of 40 is probably quite fragile wrt to performance
                                              // should better do some auto-tuning or a self-oblivious algorithm
@@ -174,6 +183,152 @@ final class T extends CallFactory {
             final int rowoffset = n - epsilon;
             for (;i < iend;) {
                 res.set(i, a.getRef(j));
+                j += n;
+                i ++;
+                if (j > size1) {
+                    j -= rsize1;
+                    i += rowoffset;
+                }
+            }
+        }
+    }
+
+    // FIXME: copy-paste of the generic version, but it pays off (e.g. b25-prog-2)
+    public static void squareBlocked(int[] a, int n, int[] res) {
+        final int delta = n > 1000 ? 40 : 0; // FIXME: the choice of 40 is probably quite fragile wrt to performance
+                                             // should better do some auto-tuning or a self-oblivious algorithm
+        final int size = n * n;
+        final int size1 = size - 1;
+        int i = 0;
+        int j = 0;
+
+        // will be transposing by blocks delta x delta
+        // reading a by rows, writing to a by columns, block-wise
+        // i is offset into res, j is offset into a
+        final int blocksPerSide = delta > 0 ? n / delta : 0;
+        final int epsilon = n - blocksPerSide * delta;
+
+        final int iadvance1 = n - delta;
+        final int jadvance1 = - ((delta * n) - 1);
+        final int iadvance2 = - (delta * (n - 1));
+        final int jadvance2 = delta * (n - 1);
+        final int iadvance3 = (delta - 1) * n + epsilon;
+        final int jadvance3 = -((n - epsilon) * n - delta);
+
+        for (int bcol = 0; bcol < blocksPerSide; bcol++) {
+            for (int brow = 0; brow < blocksPerSide; brow++) {
+                for (int icol = 0; icol < delta; icol++) {
+                    for (int irow = 0; irow < delta; irow++) {
+                        res[i] = a[j];
+                        i ++;
+                        j += n;
+                    }
+                    i += iadvance1;
+                    j += jadvance1;
+                }
+                i += iadvance2;
+                j += jadvance2;
+            }
+            i += iadvance3;
+            j += jadvance3;
+        }
+
+
+        // transponse the remainder of the matrix
+
+        if (epsilon > 0) {
+            // transpose the lower remainder (epsilon * n) into the right remainder (n * epsilon)
+            i = (n - epsilon) * n;
+            j = n - epsilon;
+            for (; i < size; i++) {
+                res[i] = a[j];
+                j += n;
+                if (j > size1) {
+                    j -= size1;
+                }
+            }
+
+            // transpose the upper-right remainder ( (n-epsilon) * epsilon) into the bottom-left remainder (epsilon * (n-epsilon))
+            i = n - epsilon;
+            j = (n - epsilon) * n;
+            final int iend = j;
+            final int rsize1 = n * epsilon - 1;
+            final int rowoffset = n - epsilon;
+            for (;i < iend;) {
+                res[i] = a[j];
+                j += n;
+                i ++;
+                if (j > size1) {
+                    j -= rsize1;
+                    i += rowoffset;
+                }
+            }
+        }
+    }
+
+ // FIXME: copy-paste of the generic version, but it pays off (e.g. b25-prog-2)
+    public static void squareBlocked(double[] a, int n, double[] res) {
+        final int delta = n > 1000 ? 40 : 0; // FIXME: the choice of 40 is probably quite fragile wrt to performance
+                                             // should better do some auto-tuning or a self-oblivious algorithm
+        final int size = n * n;
+        final int size1 = size - 1;
+        int i = 0;
+        int j = 0;
+
+        // will be transposing by blocks delta x delta
+        // reading a by rows, writing to a by columns, block-wise
+        // i is offset into res, j is offset into a
+        final int blocksPerSide = delta > 0 ? n / delta : 0;
+        final int epsilon = n - blocksPerSide * delta;
+
+        final int iadvance1 = n - delta;
+        final int jadvance1 = - ((delta * n) - 1);
+        final int iadvance2 = - (delta * (n - 1));
+        final int jadvance2 = delta * (n - 1);
+        final int iadvance3 = (delta - 1) * n + epsilon;
+        final int jadvance3 = -((n - epsilon) * n - delta);
+
+        for (int bcol = 0; bcol < blocksPerSide; bcol++) {
+            for (int brow = 0; brow < blocksPerSide; brow++) {
+                for (int icol = 0; icol < delta; icol++) {
+                    for (int irow = 0; irow < delta; irow++) {
+                        res[i] = a[j];
+                        i ++;
+                        j += n;
+                    }
+                    i += iadvance1;
+                    j += jadvance1;
+                }
+                i += iadvance2;
+                j += jadvance2;
+            }
+            i += iadvance3;
+            j += jadvance3;
+        }
+
+
+        // transponse the remainder of the matrix
+
+        if (epsilon > 0) {
+            // transpose the lower remainder (epsilon * n) into the right remainder (n * epsilon)
+            i = (n - epsilon) * n;
+            j = n - epsilon;
+            for (; i < size; i++) {
+                res[i] = a[j];
+                j += n;
+                if (j > size1) {
+                    j -= size1;
+                }
+            }
+
+            // transpose the upper-right remainder ( (n-epsilon) * epsilon) into the bottom-left remainder (epsilon * (n-epsilon))
+            i = n - epsilon;
+            j = (n - epsilon) * n;
+            final int iend = j;
+            final int rsize1 = n * epsilon - 1;
+            final int rowoffset = n - epsilon;
+            for (;i < iend;) {
+                res[i] = a[j];
                 j += n;
                 i ++;
                 if (j > size1) {
