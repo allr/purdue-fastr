fc212269d6cfc354675a1d6fe296a6d34d2939a9
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-04-29 11:14:20 +0200

    cor optimization for no NA/NaN case

--- src/r/builtins/Cor.java
+++ src/r/builtins/Cor.java
@@ -61,46 +61,33 @@ public class Cor extends CallFactory {
                     double[] res = new double[ncol * ncol];
                     double[] x = xd.getContent();
 
-                    boolean[] hasNA = columnsNAMap(x, nrow, ncol);
-                    double[] colMeans = columnMeans(x, nrow, ncol, hasNA);
+                    boolean[] hasNA = new boolean[ncol];
+                    boolean anyNA = columnsNAMap(x, nrow, ncol, hasNA);
                     int n1 = nrow - 1;
+                    boolean sdZero = false;
 
-                    for (int i = 0; i < ncol ; i++) {
-                        if (hasNA[i]) {
-                            for (int j = 0; j <= i; j++ ) {
-                                res[j * ncol + i] = RDouble.NA; // FIXME: break this into two loops?
-                                res[i * ncol + j] = RDouble.NA;
+                    if (!anyNA) {
+                        double[] colMeans = columnMeansNoNA(x, nrow, ncol);
+                        for (int i = 0; i < ncol ; i++) {
+                            double imean = colMeans[i];
+                            int ioffset = i * nrow;
+                            for (int j = 0; j <= i; j++) {
+                                double jmean = colMeans[j];
+                                int joffset = j * nrow;
+                                double sum = 0;
+                                for (int k = 0; k < nrow; k++) {
+                                    sum += (x[ioffset + k] - imean) * (x[joffset + k] - jmean);
+                                }
+                                double tmp = sum / n1;
+                                res[j * ncol + i] = tmp;
+                                res[i * ncol + j] = tmp;
                             }
-                            continue;
                         }
-                        double imean = colMeans[i];
-                        int ioffset = i * nrow;
-                        for (int j = 0; j <= i; j++) {
-                            if (hasNA[j]) {
-                                res[j * ncol + i] = RDouble.NA;
-                                res[i * ncol + j] = RDouble.NA;
-                                continue;
-                            }
-                            double jmean = colMeans[j];
-                            int joffset = j * nrow;
-                            double sum = 0;
-                            for (int k = 0; k < nrow; k++) {
-                                sum += (x[ioffset + k] - imean) * (x[joffset + k] - jmean);
-                            }
-                            double tmp = sum / n1;
-                            res[j * ncol + i] = tmp;
-                            res[i * ncol + j] = tmp;
-                        }
-                    }
-                    double[] srcov = colMeans;  // colMeans no longer needed
-                    for (int i = 0; i < ncol; i++) {
-                        if (!hasNA[i]) {
+                        double[] srcov = colMeans;  // colMeans no longer needed
+                        for (int i = 0; i < ncol; i++) {
                             srcov[i] = Math.sqrt(res[i * ncol + i]);
                         }
-                    }
-                    boolean sdZero = false;
-                    for (int i = 0; i < ncol; i++) {
-                        if (!hasNA[i]) {
+                        for (int i = 0; i < ncol; i++) {
                             for (int j = 0; j < i; j++) {
                                 if (srcov[i] == 0 || srcov[j] == 0) {
                                     sdZero = true;
@@ -115,8 +102,62 @@ public class Cor extends CallFactory {
                                     res[i * ncol + j] = tmp;
                                 }
                             }
+                            res[i * ncol + i] = 1;
                         }
-                        res[i * ncol + i] = 1;
+                    } else {
+                        double[] colMeans = columnMeans(x, nrow, ncol, hasNA);
+                        for (int i = 0; i < ncol ; i++) {
+                            if (hasNA[i]) {
+                                for (int j = 0; j <= i; j++ ) {
+                                    res[j * ncol + i] = RDouble.NA; // FIXME: break this into two loops?
+                                    res[i * ncol + j] = RDouble.NA;
+                                }
+                                continue;
+                            }
+                            double imean = colMeans[i];
+                            int ioffset = i * nrow;
+                            for (int j = 0; j <= i; j++) {
+                                if (hasNA[j]) {
+                                    res[j * ncol + i] = RDouble.NA;
+                                    res[i * ncol + j] = RDouble.NA;
+                                    continue;
+                                }
+                                double jmean = colMeans[j];
+                                int joffset = j * nrow;
+                                double sum = 0;
+                                for (int k = 0; k < nrow; k++) {
+                                    sum += (x[ioffset + k] - imean) * (x[joffset + k] - jmean);
+                                }
+                                double tmp = sum / n1;
+                                res[j * ncol + i] = tmp;
+                                res[i * ncol + j] = tmp;
+                            }
+                        }
+                        double[] srcov = colMeans;  // colMeans no longer needed
+                        for (int i = 0; i < ncol; i++) {
+                            if (!hasNA[i]) {
+                                srcov[i] = Math.sqrt(res[i * ncol + i]);
+                            }
+                        }
+                        for (int i = 0; i < ncol; i++) {
+                            if (!hasNA[i]) {
+                                for (int j = 0; j < i; j++) {
+                                    if (srcov[i] == 0 || srcov[j] == 0) {
+                                        sdZero = true;
+                                        res[j * ncol + i] = RDouble.NA;
+                                        res[i * ncol + j] = RDouble.NA;
+                                    } else {
+                                        double tmp = res[i * ncol + j]  / (srcov[i] * srcov[j]);
+                                        if (tmp > 1) {
+                                            tmp = 1;
+                                        }
+                                        res[j * ncol + i] = tmp;
+                                        res[i * ncol + j] = tmp;
+                                    }
+                                }
+                            }
+                            res[i * ncol + i] = 1;
+                        }
                     }
                     if (sdZero) {
                         RContext.warning(ast, RError.SD_ZERO);
@@ -134,18 +175,19 @@ public class Cor extends CallFactory {
         return null;
     }
 
-    public static boolean[] columnsNAMap(double[] x, int nrow, int ncol) {
-        boolean[] res = new boolean[ncol];
+    public static boolean columnsNAMap(double[] x, int nrow, int ncol, boolean[] hasNA) {
+        boolean anyNA = false;
         for (int j = 0; j < ncol; j++) {
             int offset = j * nrow;
             for (int i = 0; i < nrow; i++) {
                 if (RDouble.RDoubleUtils.isNAorNaN(x[offset + i])) {
-                    res[j] = true;
+                    hasNA[j] = true;
+                    anyNA = true;
                     break;
                 }
             }
         }
-        return res;
+        return anyNA;
     }
 
     public static double[] columnMeans(double[] x, int nrow, int ncol, boolean[] hasNA) {
@@ -155,6 +197,27 @@ public class Cor extends CallFactory {
                 res[j] = RDouble.NA;
                 continue;
             }
+            int offset = j * nrow;
+            double sum = 0;
+            for (int k = 0; k < nrow; k++) {
+                sum += x[offset + k];
+            }
+            double tmp = sum / nrow;
+            if (RDouble.RDoubleUtils.isFinite(tmp)) {
+                sum = 0;
+                for (int k = 0; k < nrow; k++) {
+                    sum += x[offset + k] - tmp;
+                }
+                tmp += sum / nrow;
+            }
+            res[j] = tmp;
+        }
+        return res;
+    }
+
+    public static double[] columnMeansNoNA(double[] x, int nrow, int ncol) {
+        double[] res = new double[ncol];
+        for (int j = 0; j < ncol; j++) {
             int offset = j * nrow;
             double sum = 0;
             for (int k = 0; k < nrow; k++) {
