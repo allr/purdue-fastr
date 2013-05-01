144fc121f2a9043c69ad9542a5647f24e49d327f
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-05-01 15:38:42 +0200

    specialization for builtin call

--- src/r/builtins/Abs.java
+++ src/r/builtins/Abs.java
@@ -106,6 +106,7 @@ public class Abs extends CallFactory {
                     int i = ((ScalarIntImpl) val).getInt();
                     return (i < 0) ? RInt.RIntFactory.getScalar(-i) : val; // NOTE: this also works with NA, NA will remain NA
                 } catch(UnexpectedResultException e) {
+                    replaceChild(exprs[0], expr); // keep the new child
                     replace(createGeneric(call, names, exprs));
                     return generic((RAny) val);
                 }
