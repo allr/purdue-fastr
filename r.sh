455fdc509b81a1004ffa191da85dbeee728ecee0
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-05-29 10:11:53 +0200

    added jlib2.jar to the executable script

--- r.sh
+++ r.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -classpath bin:lib/antlr-runtime-3.5.jar:lib/arpack_combined_all.jar:lib/junit-4.8.jar:lib/netlib-java-0.9.3.jar:lib/truffle-api-03-16-2013.jar -ea -esa r.Console $*
+java -classpath bin:lib/antlr-runtime-3.5.jar:lib/arpack_combined_all.jar:lib/junit-4.8.jar:lib/netlib-java-0.9.3.jar:lib/truffle-api-03-16-2013.jar:lib/jline-2.12.jar -ea -esa r.Console $*
