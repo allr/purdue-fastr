c211a468280262a13874a6681e72e4a3613d6334
Petr <peeetax@gmail.com>
2013-08-20 21:14:47 +0200

    Tests for node visitor completed.

--- test/r/analysis/codegen/FastrLoaderVisitorTest.java
+++ test/r/analysis/codegen/FastrLoaderVisitorTest.java
@@ -40,7 +40,15 @@ class DoubleNestedNode extends Node {
         this.child1 = adoptChild(child1);
         this.child2 = adoptChild(child2);
     }
+}
 
+class ArrayNestedNode extends Node {
+    Node[] children;
+    public ArrayNestedNode(int size) {
+        children = new Node[size];
+        for (int i = 0; i < size; ++i)
+            children[i] = adoptChild(new Node());
+    }
 }
 
 
@@ -124,13 +132,94 @@ public class FastrLoaderVisitorTest {
 
     }
 
+    abstract static class CountingNodeVisitor implements NodeVisitor {
+        int[] count;
+    }
+
     @Test
     public void visitorWorksOnTrees() {
         final Node n = new Node();
+        final DoubleNestedNode n2 = new DoubleNestedNode(n, n);
+        final DoubleNestedNode n3 = new DoubleNestedNode(n2, n2);
+        final DoubleNestedNode n4 = new DoubleNestedNode(n3, n);
+        CountingNodeVisitor v = new CountingNodeVisitor() {
+            @Override
+            public boolean visit(RNode node) {
+                if (node == n)
+                    count[0] += 1;
+                else if (node == n2)
+                    count[1] += 1;
+                else if (node == n3)
+                    count[2] += 1;
+                else
+                    count[3] += 1;
+                return true;
+            }
+
+            {
+                count = new int[4];
+            }
+        };
+        n4.accept(v);
+        assertTrue(v.count[3] == 1);
+        assertTrue(v.count[2] == 1);
+        assertTrue(v.count[1] == 2);
+        assertTrue(v.count[0] == 5);
     }
 
+    @Test
+    public void visitorWorksOnArrays() {
+        final ArrayNestedNode n = new ArrayNestedNode(10);
+        n.accept(new NodeVisitor() {
+            int i = -1;
+            @Override
+            public boolean visit(RNode node) {
+                if (i == -1)
+                    assertTrue(node == n);
+                else
+                    assertTrue(node == n.children[i]);
+                ++i;
+                return true;
+            }
+        });
+    }
 
+    @Test
+    public void visitorWorksOnNullArrays() {
+        final ArrayNestedNode n = new ArrayNestedNode(10);
+        n.children = null;
+        n.accept(new NodeVisitor() {
+            int i = -1;
+            @Override
+            public boolean visit(RNode node) {
+                assertTrue(node == n);
+                return true;
+            }
+        });
+    }
 
+    @Test
+    public void visitorWorksOnArraysWithNulls() {
+        final ArrayNestedNode n = new ArrayNestedNode(10);
+        n.children[5] = null;
+        n.children[8] = null;
+        n.accept(new NodeVisitor() {
+            int i = -1;
+            @Override
+            public boolean visit(RNode node) {
+                if (i == -1)
+                    assertTrue(node == n);
+                else
+                    assertTrue(node == n.children[i]);
+                ++i;
+                if (i == 5)
+                    ++i;
+                else if (i == 8)
+                    ++i;
+                return true;
+            }
+        });
+    }
 }
 
 
