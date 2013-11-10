15580ee27fbd9f533ada8ea36b750462b7874094
Tomas Kalibera <kalibera@cs.purdue.edu>
2013-11-10 01:40:32 -1000

    added missing javaassist.jar

--- nr.sh
+++ nr.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 java -Xmx8g -Djava.library.path=native:native/netlib-java/jni \
-  -classpath bin:lib/antlr-runtime-3.5.jar:lib/arpack_combined_all.jar:lib/junit-4.8.jar:lib/netlib-java-0.9.3.jar:lib/jline-2.12.jar \
+  -classpath bin:lib/antlr-runtime-3.5.jar:lib/arpack_combined_all.jar:lib/junit-4.8.jar:lib/netlib-java-0.9.3.jar:lib/jline-2.12.jar:lib/javassist.jar \
    r.Console $*
   
 # -ea -esa
